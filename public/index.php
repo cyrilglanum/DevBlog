61e02e9eea1b0f0798e67805d460d23a20e00ea0
Cyril <cyril@glanum.com>
2021-06-16 16:36:50 +0200

    Ajout du controllerResolver

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
@@ -15,15 +16,14 @@ $context = new RequestContext();
 $context->fromRequest($request);
 
 $urlMatcher = new UrlMatcher($routes, $context);
+$controllerResolver = new ControllerResolver();
 
 try {
     $resultat = $urlMatcher->match($request->getPathInfo());
     $request->attributes->add($resultat);
 
-    $className = substr($resultat['_controller'],0,strpos($resultat['_controller'],'@'));
-    $methodName = substr($resultat['_controller'],strpos($resultat['_controller'],'@')+1);
+    $controller = $controllerResolver->getController($request);
 
-    $controller = [new $className, $methodName];
     $response = call_user_func($controller,$request);
 } catch (ResourceNotFoundException $e) {
     $response = new Response("La page demandée n'existe pas", 404);
