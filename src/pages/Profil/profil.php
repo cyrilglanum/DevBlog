a2050f9a333fb3f6a74779b08f64dafc18265018
Cyril <cyril@glanum.com>
2021-07-05 10:32:19 +0200

    Profil page

--- src/pages/Profil/profil.php
+++ src/pages/Profil/profil.php
@@ -44,10 +44,6 @@ require $dir . 'src/pages/partials/header.php';
                             <label for="exampleInputPassword1">Password</label>
                             <input type="password" class="form-control" id="exampleInputPassword1">
                         </div>
-                        <div class="form-group form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                     <?php
