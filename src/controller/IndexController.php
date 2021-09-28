33ea617f69fe5a9ebfdb32767f8174ad44a189cc
Cyril <cyril@glanum.com>
2021-09-28 14:40:56 +0200

    P5 : deletion de PHP mailer + ajout de mail php

--- src/controller/IndexController.php
+++ src/controller/IndexController.php
@@ -59,47 +59,12 @@ class IndexController extends BaseRepository
 
         $repo = new MessageRepository();
         $repo->saveMessage($messageToSave);
-mail('cyril@glanum.com', 'test', 'test');
-        $mail = new PHPMailer(true);
-        try {
-            //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host = 'smtp-pulse.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-            $mail->Username = 'cyril@glanum.com';                     //SMTP username
-            $mail->Password = '24teg7XqSK6pBP';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port = 2525;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
-            //Recipients
-            $mail->setFrom('from@example.com', 'Mailer');
-            $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
-            $mail->addAddress('ellen@example.com');               //Name is optional
-            $mail->addReplyTo('info@example.com', 'Information');
-            $mail->addCC('cc@example.com');
-            $mail->addBCC('bcc@example.com');
+        mail('cyril@glanum.com', 'test', 'test');
 
-            //Attachments
-//            $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-//            $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-            $mail->send();
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
-        }
-
-        dd($mail->smtpConnect());
-//        ob_start();
-//        include __DIR__ . '/../pages/validation/redirectHome.php';
-//        return new Response(ob_get_clean());
+        ob_start();
+        include __DIR__ . '/../pages/validation/redirectHome.php';
+        return new Response(ob_get_clean());
 
     }
 }
